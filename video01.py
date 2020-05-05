9f84d549842f5f75aed40930602295b07695abc9
XS_Jinbert <mr_jinbert@163.com>
2020-05-05 14:56:55 +0800

    Create project

---
+++ video01.py
@@ -0,0 +1,8 @@
+# -*- coding = utf-8 -*-
+
+import cv2
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
+
+savepath = r"D:\projects\Python\VideoToChars\badapple"
+videopath = r"D:\projects\Python\VideoToChars\badapple.mp4"
